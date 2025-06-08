b5d222a97ad6982aa878537439294dc01c4139c0
IndieFantasyClub <indiefantasyclub@gmail.com>
2025-06-08 13:59:10 -0600

    Create App.js

---
+++ public/src/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SwipePage from './SwipePage';
+
+function App() {
+  return (
+    <div>
+      <h1>BetaMatch</h1>
+      <SwipePage />
+    </div>
+  );
+}
+
+export default App;
